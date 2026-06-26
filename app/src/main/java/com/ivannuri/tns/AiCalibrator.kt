d9f80bff3f7a22b3c45896caf6c12c8f6934a3ef
luisurielpimentelperez814-design <luisurielpimentelperez814@gmail.com>
2026-06-26 13:44:07 -0600

    Update AiCalibrator.kt

--- app/src/main/java/com/ivannuri/tns/AiCalibrator.kt
+++ app/src/main/java/com/ivannuri/tns/AiCalibrator.kt
@@ -1,2 +1,9 @@
 package com.ivannuri.tns
-class AiCalibrator
+
+class AiCalibrator {
+    fun calibrate(): String {
+        // Aquí conectaremos tu backend Flask en Render
+        // Por ahora simula 96kHz
+        return "IA lista - 96kHz/32-bit activado"
+    }
+}
